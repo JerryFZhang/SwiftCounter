1ff7d33df9073dae5548d190d0748778bf091638
zhang96 <zhang96@users.noreply.github.com>
2016-01-13 14:58:33 -0500

    Label and Controller Added
    Added a label and a controller for the counter app.

---
+++ Counter/CounterViewController.swift
@@ -0,0 +1,20 @@
+
+//
+//  CounterViewControllwe.swift
+//  Counter
+//
+//  Created by Jerry Zhang on 2016-01-13.
+//  Copyright © 2016 Jerry Zhang. All rights reserved.
+//
+
+import Foundation
+
+class CounterViewController{
+    
+    
+    
+    
+    
+    
+    
+}
