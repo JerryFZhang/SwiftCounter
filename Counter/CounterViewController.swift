118a790dbe846a1a45956167732f7683d3068d8f
zhang96 <zhang96@users.noreply.github.com>
2016-01-20 16:11:41 -0500

    Deleted the ViewController.swift
    Deleted the ViewController.swift since it wasn't used

--- Counter/CounterViewController.swift
+++ Counter/CounterViewController.swift
@@ -1,33 +1,49 @@
-
 //
-//  CounterViewControllwe.swift
+//  ViewController.swift
 //  Counter
 //
-//  Created by Jerry Zhang on 2016-01-13.
+//  Created by Jerry Zhang on 2016-01-12.
 //  Copyright © 2016 Jerry Zhang. All rights reserved.
 //
 
-import Foundation
 import UIKit
+import Foundation
 
-class CounterViewController : UIViewController {
 
-    
-    @IBOutlet weak var number: UILabel!
+class CounterViewController : UIViewController{
+
+    @IBOutlet weak var Result: UILabel!
     
     var counter = 0
     
-    @IBAction func touch(sender: AnyObject) {
-        counter++
-        number.text = String(counter)
-        
-    
-    }
-    
-    @IBAction func Clear(sender: AnyObject) {
+    @IBAction func Reset(sender: AnyObject) {
+   
         counter = 0
-        number.text = String(counter)
+        Result.text = String(counter)
         NSLog("Cleared")
+    
     }
     
+    @IBAction func Increment(sender: AnyObject) {
+        
+        counter++
+        Result.text = String(counter)
+        NSLog("Incremented")
+    
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
 }
+
